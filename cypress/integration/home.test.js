4fea197322145ec165cbd63f7c6c51708861f248
Mohamed_Elsafty <mohamedelsafty135@gmail.com>
2024-09-09 21:21:24 +0300

    update

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,64 +1,33 @@
+describe("Home Page", () => {
+  beforeEach(() => {
+    cy.fixture("courses.json").as("coursesJSON");
 
+    cy.server();
 
+    cy.route("/api/courses", "@coursesJSON").as("courses");
 
-describe('Home Page', () => {
+    cy.visit("/");
+  });
 
-    beforeEach(() => {
+  it("should display a list of courses", () => {
+    cy.contains("All Courses");
 
-        cy.fixture('courses.json').as("coursesJSON");
+    cy.wait("@courses");
 
-        cy.server();
+    cy.get("mat-card").should("have.length", 9);
+  });
 
-        cy.route('/api/courses', "@coursesJSON").as("courses");
+  it("should display the advanced courses", () => {
+    cy.get(".mat-mdc-tab").should("have.length", 2);
 
-        cy.visit('/');
+    cy.get(".mat-mdc-tab").last().click();
 
-    });
+    cy.get(".mat-mdc-tab-body-active .mat-mdc-card-title")
+      .its("length")
+      .should("be.gt", 1);
 
-    it('should display a list of courses', () => {
-
-        cy.contains("All Courses");
-
-        cy.wait('@courses');
-
-        cy.get("mat-card").should("have.length", 9);
-
-    });
-
-    it('should display the advanced courses', () => {
-
-        cy.get('.mat-tab-label').should("have.length", 2);
-
-        cy.get('.mat-tab-label').last().click();
-
-        cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
-
-        cy.get('.mat-tab-body-active .mat-card-title').first()
-            .should('contain', "Angular Security Course");
-
-    });
-
-
+    cy.get(".mat-mdc-tab-body-active .mat-mdc-card-title")
+      .first()
+      .should("contain", "Angular Security Course");
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
